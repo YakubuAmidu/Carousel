2ed6d90a7e708dec3d7452b6a92cabcbf65aea86
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-01 02:53:44 -0700

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Card(props){
+    return(
+        <div></div>
+    )
+}
+
+const styles = {
+    card: {},
+    card_img: {}
+}
+
+export default React.memo(Card);
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Card from './Card';
 
 class Deck extends Component{
     constructor(props){
-        super(props);
-        this.state = {}
+        super(props)
+        this.state = {
+            cards: []
+        }
     }
     render(){
         return(
-            <div>
-                <Card />
-            </div>
+            <Fragment>
+                <div style={styles.view_port}>
+                    <div style={styles.images_container}>
+                        {this.state.cards}
+                    </div>
+                </div>
+            </Fragment>
         )
     }
+}
+
+const styles = {
+    view_port: {},
+    images_container: {}
 }
 
 export default Deck;
